f7259e174af99d147fba7a5eadbe9b6232f4ec0c
sharathdoes <justabountyhunter935@gmail.com>
2025-09-30 21:50:28 +0530

    anonymous-fix

--- app/actions/feedback.ts
+++ app/actions/feedback.ts
@@ -2,7 +2,7 @@
 import prisma from "@/prisma/prisma";
 
 export const submitFeedback=async({userId, anonymous,rating,feedback}:{userId:string, anonymous:boolean, rating:number, feedback:string})=>{
-    if(!userId||!anonymous||!rating||!feedback) throw new Error("More Details is required");
+    if(!userId||!rating||!feedback) throw new Error("More Details is required");
     const feedbaresck =await prisma.feedback.create({
         data:{
             userId, anonymous, rating, feedback
